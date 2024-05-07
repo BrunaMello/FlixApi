5cb72d3f6e38a71715128e7e61cd57eb370e1703
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-05-07 18:43:30 +0100

    129 - App actors e choice fields

--- FlixApi/urls.py
+++ FlixApi/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
 	path('genres/<int:pk>', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
+	# path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
+	# path('actors/<int:pk>', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view'),
+
 ]
