e14dcecbc5fb63e2dc7289427289d422c678690f
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-05-09 18:48:20 +0100

    143 - ModelSerializer e Validações

--- movies/serializers.py
+++ movies/serializers.py
@@ -3,7 +3,17 @@ from rest_framework import serializers
 from movies.models import Movie
 
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieModelSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Movie
 		fields = '__all__'
+
+	def validate_release_date(self, value):
+		if value.year < 1900:
+			raise serializers.ValidationError('Release date must be after 1900')
+		return value
+
+	def validate_resume(self, value):
+		if len(value) > 200:
+			raise serializers.ValidationError('Comment must be below 200 characters long')
+		return value
