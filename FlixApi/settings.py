0fb5b97c22091aea408c6a6f70240e696df35acb
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-05-13 18:59:25 +0100

    150 – Implementando autenticação JWT na nossa Api

--- FlixApi/settings.py
+++ FlixApi/settings.py
@@ -36,11 +36,12 @@ INSTALLED_APPS = [
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
 	'rest_framework',
+	'rest_framework_simplejwt',
 	'genres.apps.GenresConfig',
 	'actors.apps.ActorsConfig',
 	'movies.apps.MoviesConfig',
-	"reviews.apps.ReviewsConfig"
-
+	"reviews.apps.ReviewsConfig",
+	"authentication.apps.AuthenticationConfig"
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+	'DEFAULT_AUTHENTICATION_CLASSES': (
+		'rest_framework_simplejwt.authentication.JWTAuthentication',
+	)
+}
