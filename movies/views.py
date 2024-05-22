8e70d7db91a690562673242623f7684ec7fa2b77
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-05-22 11:52:45 +0100

    163 - Endpoint de estatísticas (parte 1)

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 
 from FlixApi.permissions import GlobalDefaultPermission
@@ -16,3 +16,14 @@ class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
 	permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
 	queryset = Movie.objects.all()
 	serializer_class = MovieModelSerializer
+
+
+class MovieStatsView(views.APIView):
+	permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+	queryset = Movie.objects.all()
+
+	def get(self, request):
+		return response.Response(
+			data={'message': 'Movie stats'},
+			status=status.HTTP_200_OK
+		)
