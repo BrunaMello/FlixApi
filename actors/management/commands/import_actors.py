a32636cff1dfde9e3ae123ddfb41b9907a321bee
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-05-27 18:34:02 +0100

    174 - Django command (parte 3)

--- actors/management/commands/import_actors.py
+++ actors/management/commands/import_actors.py
@@ -1,4 +1,9 @@
+import csv
+from datetime import datetime
+
 from django.core.management.base import BaseCommand
+
+from actors.models import Actor
 
 
 class Command(BaseCommand):
@@ -6,16 +11,26 @@ class Command(BaseCommand):
 	def add_arguments(self, parser):
 		parser.add_argument(
 			'file_name',
-			type=int,
-			help='The file name to import actors from'
-		)
-		parser.add_argument(
-			'file_name2',
-			type=int,
-			help='The file name to import actors from'
+			type=str,
+			help='Nome do arquivo CSV com atores'
 		)
 
 	def handle(self, *args, **options):
 		file_name = options['file_name']
-		file_name2 = options['file_name2']
-		print(f'Command executed! {file_name}')
+
+		with open(file_name, 'r', encoding='utf-8') as file:
+			reader = csv.DictReader(file)
+			for row in reader:
+				name = row['name']
+				birthday = datetime.strptime(row['birthday'], '%Y-%m-%d').date()
+				nationality = row['nationality']
+
+				self.stdout.write(self.style.NOTICE(name))
+
+				Actor.objects.create(
+					name=name,
+					birthday=birthday,
+					nationality=nationality
+				)
+
+		self.stdout.write(self.style.SUCCESS('Atores importados com sucesso!'))
